7ba4dc5277f7f58f77b832fb43ae18035ed12421
magdaclerin <102517195+magdaclerin@users.noreply.github.com>
2022-08-22 11:48:48 +0200

    Add files via upload

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Hello Wilders, welcome to my application!')
 
-name = st.text_input("Please give me your name :")
-name_length = len(name)
-st.write("Your name has ",name_length,"characters")
+st.write("I enjoy to discover stremalit possibilities")
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_cars = pd.read_csv(link)
+
+# Here we use "magic commands":
+df_cars
+#%%
