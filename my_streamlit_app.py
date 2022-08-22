9ddd97b40c2b64ca260a0da8ad57b47192300a81
magdaclerin <102517195+magdaclerin@users.noreply.github.com>
2022-08-22 11:23:47 +0200

    second load

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -5,3 +5,5 @@ st.title('Hello Wilders, welcome to my application!')
 name = st.text_input("Please give me your name :")
 name_length = len(name)
 st.write("Your name has ",name_length,"characters")
+
+/Users/Magda/Desktop/migration_test/wild_code_school/my_streamlit_app.py
