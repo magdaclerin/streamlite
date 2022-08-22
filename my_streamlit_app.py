5d8435f5a952d6e8b146ec63a6d7a4986257417a
magdaclerin <102517195+magdaclerin@users.noreply.github.com>
2022-08-22 11:51:03 +0200

    Add files via upload

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title('Hello Wilders, welcome to my application!')
 
-st.write("I enjoy to discover stremalit possibilities")
+st.write("I enjoy to discover stremalit possibilities :)))) ")
 
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_cars = pd.read_csv(link)
